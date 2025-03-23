597ff259a94d4cf19440c82802d99aea5364b05f
Nadya <nadyasekarrahmawati@gmail.com>
2025-03-24 02:46:46 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300020/Program.cs
+++ tpmodul6_103022300020/Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Dunia!");
+﻿using System;
+using System.Diagnostics;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Debug.Assert(title != null && title.Length <= 100, "Judul video tidak boleh null atau lebih dari 100 karakter.");
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999); // masukkan ID random 5 digit
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        Debug.Assert(count > 0 && count <= 10000000, "Play count harus lebih dari 0 dan tidak boleh lebih dari 10.000.000.");
+
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count terlalu besar.");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Nama Praktikan]");
+        video.IncreasePlayCount(500);
+        video.PrintVideoDetails();
+    }
+}
